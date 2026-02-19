181748abbb842d74095354118d7dcb9048b37bbd
Himanshu pandey <sahilpandey765@gmail.com>
2026-02-19 14:35:05 +0530

    Enhance schedule generation with project selection
    Added functionality to select and display scheduled projects for the week.

--- Scheduler.java
+++ Scheduler.java
@@ -1,6 +1,6 @@
 package com.promanage;
 
-import java.util.List;
+import java.util.*;
 
 public class Scheduler {
 
@@ -9,31 +9,73 @@ public class Scheduler {
         int n = projects.size();
         int maxDays = 5;
 
-        
         int[][] dp = new int[n + 1][maxDays + 1];
 
-        
+        // Fill DP table
         for (int i = 1; i <= n; i++) {
-
             Project p = projects.get(i - 1);
 
             for (int d = 1; d <= maxDays; d++) {
 
-                
                 if (p.getDeadline() >= d) {
-
                     dp[i][d] = Math.max(
-                            dp[i - 1][d],   
-                            p.getRevenue() + dp[i - 1][d - 1]  
+                            dp[i - 1][d],
+                            p.getRevenue() + dp[i - 1][d - 1]
                     );
-
                 } else {
                     dp[i][d] = dp[i - 1][d];
                 }
             }
         }
 
-        System.out.println("\n Using Dynamic Programming ");
+        System.out.println("\nUsing Dynamic Programming");
         System.out.println("Maximum Revenue Earned: " + dp[n][maxDays]);
+
+
+
+        List<Project> selectedProjects = new ArrayList<>();
+
+        int i = n;
+        int d = maxDays;
+
+        while (i > 0 && d > 0) {
+
+            if (dp[i][d] != dp[i - 1][d]) {
+                Project p = projects.get(i - 1);
+                selectedProjects.add(p);
+                d = d - 1;
+            }
+
+            i--;
+        }
+
+
+
+        Project[] week = new Project[maxDays];
+        String[] days = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday"};
+
+        for (Project p : selectedProjects) {
+
+            int lastDay = Math.min(p.getDeadline(), maxDays) - 1;
+
+            for (int j = lastDay; j >= 0; j--) {
+                if (week[j] == null) {
+                    week[j] = p;
+                    break;
+                }
+            }
+        }
+
+        System.out.println("\n Weekly Schedule ");
+
+        for (int k = 0; k < maxDays; k++) {
+            if (week[k] != null) {
+                System.out.println(days[k] + " -> " +
+                        week[k].getTitle() +
+                        " (Revenue: " + week[k].getRevenue() + ")");
+            } else {
+                System.out.println(days[k] + " -> No Project");
+            }
+        }
     }
 }
